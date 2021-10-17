18777b9a905674e254231d434c12db0f77e9fa17
валера кравец <kravetz.valera2015@gmail.com>
2021-10-17 18:03:12 +0300

    Добавьте файлы проекта.

---
+++ Lab01_Kravets_V/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace Lab01_Kravets_V
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox_pass = new System.Windows.Forms.TextBox();
+            this.textBox_login = new System.Windows.Forms.TextBox();
+            this.button_OK = new System.Windows.Forms.Button();
+            this.label_OK = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.textBox_pass);
+            this.groupBox1.Controls.Add(this.textBox_login);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(180, 80);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Авторизація";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Пароль";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Логин";
+            // 
+            // textBox_pass
+            // 
+            this.textBox_pass.Location = new System.Drawing.Point(69, 50);
+            this.textBox_pass.Name = "textBox_pass";
+            this.textBox_pass.Size = new System.Drawing.Size(100, 22);
+            this.textBox_pass.TabIndex = 1;
+            this.textBox_pass.Enter += new System.EventHandler(this.textBox_pass_Enter);
+            this.textBox_pass.Leave += new System.EventHandler(this.textBox_pass_Leave);
+            // 
+            // textBox_login
+            // 
+            this.textBox_login.Location = new System.Drawing.Point(69, 22);
+            this.textBox_login.Name = "textBox_login";
+            this.textBox_login.Size = new System.Drawing.Size(100, 22);
+            this.textBox_login.TabIndex = 0;
+            this.textBox_login.Enter += new System.EventHandler(this.textBox_login_Enter);
+            this.textBox_login.Leave += new System.EventHandler(this.textBox_login_Leave);
+            // 
+            // button_OK
+            // 
+            this.button_OK.Location = new System.Drawing.Point(59, 98);
+            this.button_OK.Name = "button_OK";
+            this.button_OK.Size = new System.Drawing.Size(75, 23);
+            this.button_OK.TabIndex = 1;
+            this.button_OK.Text = "Войти";
+            this.button_OK.UseVisualStyleBackColor = true;
+            this.button_OK.Click += new System.EventHandler(this.button_OK_Click);
+            // 
+            // label_OK
+            // 
+            this.label_OK.AutoSize = true;
+            this.label_OK.Location = new System.Drawing.Point(141, 99);
+            this.label_OK.Name = "label_OK";
+            this.label_OK.Size = new System.Drawing.Size(28, 17);
+            this.label_OK.TabIndex = 2;
+            this.label_OK.Text = "OK";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(197, 127);
+            this.Controls.Add(this.label_OK);
+            this.Controls.Add(this.button_OK);
+            this.Controls.Add(this.groupBox1);
+            this.MaximumSize = new System.Drawing.Size(215, 174);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox_pass;
+        private System.Windows.Forms.TextBox textBox_login;
+        private System.Windows.Forms.Button button_OK;
+        private System.Windows.Forms.Label label_OK;
+    }
+}
+
---
+++ Lab01_Kravets_V/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab01_Kravets_V
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            textBox_login.Text = "Введіть Логін";
+            textBox_login.ForeColor = Color.Gray;
+            textBox_pass.Text = "Введіть Пароль";
+            textBox_pass.ForeColor = Color.Gray;
+        }
+
+
+
+        private void button_OK_Click(object sender, EventArgs e)
+        {
+            if(textBox_login.Text == "Valerii" && textBox_pass.Text == "88888888")
+            {
+                label_OK.BackColor = Color.Green;
+                MessageBox.Show("Hello Valerii");
+            }
+            else
+            {
+                label_OK.BackColor = Color.Red;
+                MessageBox.Show("error login or password");
+            }
+        }
+
+        private void textBox_login_Enter(object sender, EventArgs e)
+        {
+            if (textBox_login.Text == "Введіть Логін")
+            {
+                textBox_login.Text = "";
+                textBox_login.ForeColor = Color.Black;
+            }
+        }
+
+        private void textBox_login_Leave(object sender, EventArgs e)
+        {
+            if (textBox_login.Text == "")
+            {
+                textBox_login.Text = "Введіть Логін";
+                textBox_login.ForeColor = Color.Gray;
+            }
+        }
+
+        private void textBox_pass_Enter(object sender, EventArgs e)
+        {
+            if (textBox_pass.Text == "Введіть Пароль")
+            {
+                textBox_pass.Text = "";
+                textBox_pass.ForeColor = Color.Black;
+            }
+        }
+
+        private void textBox_pass_Leave(object sender, EventArgs e)
+        {
+            if (textBox_pass.Text == "")
+            {
+                textBox_pass.Text = "Введіть Пароль";
+                textBox_pass.ForeColor = Color.Gray;
+            }
+        }
+    }
+}
